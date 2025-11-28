e0e2525bb854e370263d2129aedde9992ea8d5b4
Dipo Ajayi <ajayidipo@ymail.com>
2025-11-28 17:57:53 +0100

    rename created resources

--- main.tf
+++ main.tf
@@ -7,24 +7,24 @@ resource "azurerm_dns_zone" "truefoundry_dns_zone" {
 }
 
 # Create User Assigned Managed Identity for cert-manager
-resource "azurerm_user_assigned_identity" "cert_manager_identity" {
+resource "azurerm_user_assigned_identity" "truefoundry_cert_manager_identity" {
   name                = var.truefoundry_managed_identity_name_override_enabled ? var.truefoundry_managed_identity_override_name : "${var.cluster_name}-${var.managed_identity_name}"
   resource_group_name = var.resource_group_name
   location            = var.location
   tags                = var.tags
 }
 
-resource "azurerm_role_assignment" "dns_zone_contributor" {
+resource "azurerm_role_assignment" "truefoundry_dns_zone_contributor" {
   scope                = var.use_existing_dns_zone ? data.azurerm_dns_zone.existing_zone[0].id : azurerm_dns_zone.truefoundry_dns_zone[0].id
   role_definition_name = "DNS Zone Contributor"
-  principal_id         = azurerm_user_assigned_identity.cert_manager_identity.principal_id
+  principal_id         = azurerm_user_assigned_identity.truefoundry_cert_manager_identity.principal_id
 }
 
 # Create Federated Identity Credential for Workload Identity
-resource "azurerm_federated_identity_credential" "cert_manager_federated_identity" {
+resource "azurerm_federated_identity_credential" "truefoundry_cert_manager_federated_identity" {
   name                = "cert-manager-federated-identity"
   resource_group_name = var.resource_group_name
-  parent_id           = azurerm_user_assigned_identity.cert_manager_identity.id
+  parent_id           = azurerm_user_assigned_identity.truefoundry_cert_manager_identity.id
   audience            = ["api://AzureADTokenExchange"]
   issuer              = data.azurerm_kubernetes_cluster.aks.oidc_issuer_url
   subject             = "system:serviceaccount:${var.cert_manager_namespace}:${var.cert_manager_service_account}"
