6052a19146036ad4ea556411f9c9a510585ab441
Dipo Ajayi <ajayidipo@ymail.com>
2025-12-03 12:25:07 +0100

    add workflows

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 1.10"
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "~> 4.54.0"
     }
   }
